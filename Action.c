66557d51651517a42e2a9db96ca02de6c130ea9d
AblyazovDavlat <ablyazovdavlat@gmail.com>
2019-11-28 11:40:59 +0300

    Minor release

--- Action.c
+++ Action.c
@@ -38,6 +38,11 @@ Action()
     
     int selectCount = 1;
     int selectValues = 0;
+    
+    char indexForm[2];
+    char url[STR_LEN];
+    
+    int flag, number = 0;
 
     web_set_max_html_param_len("1024");
     	
@@ -53,10 +58,13 @@ Action()
 		"Mode=HTML", 
 		LAST);
 
+
 	web_reg_find("Text=Question 1", 
 		LAST);
 
     find_elements();
+    
+   	flag = web_reg_find("Text=Test successfully passed", LAST);
 
 	lr_continue_on_error(1);
 	
@@ -67,244 +75,166 @@ Action()
 
     lr_continue_on_error(0);
     
-    for (iT = 1; iT <= atoi(lr_eval_string("{Text_count}")) ; iT++)
-    {
-		sprintf(tmpTextFields[iT], "{Text_%d}", iT);
-		strcpy(FormFields[iT].name, "Name=");
-		strcat(FormFields[iT].name, lr_eval_string(tmpTextFields[iT]));
-		strcpy(FormFields[iT].value, "Value=test");
-		lr_output_message(FormFields[iT].name);
-    }
-    
-
-    for (iR = 1; iR <= atoi(lr_eval_string("{RadioName_count}")) ; iR++)
-    {
-    	char tmpName[STR_LEN];
-    	char tmpValue[STR_LEN];
-    	char currentRadioName[STR_LEN];
+    while( !flag )
+	{    	
+		for (iT = 1; iT <= atoi(lr_eval_string("{Text_count}")) ; iT++)
+	    {
+			sprintf(tmpTextFields[iT], "{Text_%d}", iT);
+			strcpy(FormFields[iT].name, "Name=");
+			strcat(FormFields[iT].name, lr_eval_string(tmpTextFields[iT]));
+			strcpy(FormFields[iT].value, "Value=test");
+			lr_output_message(FormFields[iT].name);
+	    }
+	    
+	
+	    for (iR = 1; iR <= atoi(lr_eval_string("{RadioName_count}")) ; iR++)
+	    {
+	    	char tmpName[STR_LEN];
+	    	char tmpValue[STR_LEN];
+	    	char currentRadioName[STR_LEN];
+	    	
+	    	sprintf(tmpRadioName[iR], "{RadioName_%d}", iR);
+	    	strcpy(tmpName, lr_eval_string(tmpRadioName[iR]));
+	    	
+	    	sprintf(tmpRadioValue[iR], "{RadioValue_%d}", iR);
+	    	strcpy(tmpValue, lr_eval_string(tmpRadioValue[iR]));
+	    	
+	    	if (strcmp(currentRadioName, tmpName)!=0)
+	    	{
+	    		strcpy(RadioGroups[radioCount].name, tmpName);
+	    		strcpy(currentRadioName, RadioGroups[radioCount].name);
+	    		radioCount++;
+	    		radioValues = 0;
+	    	}
+	    	
+	    	strcpy(RadioGroups[radioCount-1].values[radioValues], tmpValue);
+	    	radioValues++;
+	    }
+	    
+	    for (iR = 1; iR < radioCount; iR++){
+	    	char max[STR_LEN];
+	    	strcpy(max,RadioGroups[iR].values[0]);
+	    	for (radioValues = 0; radioValues < 10; radioValues++)
+	    	{
+	    		if(strlen(RadioGroups[iR].values[radioValues]) > strlen(max))
+	    		{
+	    			strcpy(max,RadioGroups[iR].values[radioValues]);
+	    		}
+	    		strcpy(RadioGroups[iR].maxValue, max);
+	    	}
+	    }
+	    
+	    j = radioCount - 1;
+	    for (i = iT; i < iT + radioCount - 1; i++)
+	    {
+			strcpy(FormFields[i].name, "Name=");
+			strcat(FormFields[i].name, RadioGroups[j].name);
+			strcpy(FormFields[i].value, "Value=");
+			strcat(FormFields[i].value, RadioGroups[j].maxValue);
+			lr_output_message(FormFields[i].name);
+			lr_output_message(FormFields[i].value);
+			j--;
+	    }
+	    
+	    selectCount = atoi(lr_eval_string("{Select_count}"));
+	                       
+	    for (iS = 1; iS <= selectCount; iS++)
+	    {
+	    	int startPos = 0;
+	    	int endPos = 0;
+	    	int index, i = 0;
+	    	char tmpStr[1024];
+	    	sprintf(tmpSelectFields[iS], "{Select_%d}", iS);
+	    	strcpy(tmpStr, lr_eval_string(tmpSelectFields[iS]));
+	    	
+	    	for( index = 0; index < strlen(tmpStr); index++ )
+	    	{
+	    		if (tmpStr[index] == '"')
+	    		{
+	    			startPos = index;
+	    			while((tmpStr[index+1] != '"') && (index < strlen(tmpStr) - 1))
+	    			{
+	    				index++;
+	    			}
+	    			endPos = index;
+	    			
+	    			
+	    			if (startPos == 0)
+	    			{
+	    				strncpy(SelectBoxes[iS].name, tmpStr+startPos + 1, endPos - startPos);
+	    			}
+	    			else
+	    			{
+	    				strncpy(SelectBoxes[iS].values[i], tmpStr+startPos + 1, endPos - startPos);
+	    				i++;
+	    			}
+	    			index++;
+	    		}
+	    	}
+	    	
+	    }
+	    
+	    
+	    for (iS = 1; iS <= selectCount; iS++)
+	    {
+	    	char max[STR_LEN];
+	    	strcpy(max,SelectBoxes[iS].values[0]);
+	    	for (selectValues = 0; selectValues < 10; selectValues++)
+	    	{
+	    		if(strlen(SelectBoxes[iS].values[selectValues]) > strlen(max))
+	    		{
+	    			strcpy(max,SelectBoxes[iS].values[selectValues]);
+	    		}
+	    		strcpy(SelectBoxes[iS].maxValue, max);
+	    	}
+	    }
+	
+	    j = selectCount;
+	    for (iT = i; iT < i + selectCount; iT++)
+	    {
+			strcpy(FormFields[iT].name, "Name=");
+			strcat(FormFields[iT].name, SelectBoxes[j].name);
+			strcpy(FormFields[iT].value, "Value=");
+			strcat(FormFields[iT].value, SelectBoxes[j].maxValue);
+			lr_output_message(FormFields[iT].name);
+			lr_output_message(FormFields[iT].value);
+			strcpy(SelectBoxes[j].maxValue, "/0");
+			j--;
+	    }
+	    
+    	strcpy(FormFields[iT].name, "LAST");
     	
-    	sprintf(tmpRadioName[iR], "{RadioName_%d}", iR);
-    	strcpy(tmpName, lr_eval_string(tmpRadioName[iR]));
+    	find_elements();
     	
-    	sprintf(tmpRadioValue[iR], "{RadioValue_%d}", iR);
-    	strcpy(tmpValue, lr_eval_string(tmpRadioValue[iR]));
+    	number++;
+    	sprintf(indexForm, "%d", number);
+    	sprintf(url, "Action=http://test.youplace.net/question/%d", number);
     	
-    	if (strcmp(currentRadioName, tmpName)!=0)
-    	{
-    		strcpy(RadioGroups[radioCount].name, tmpName);
-    		strcpy(currentRadioName, RadioGroups[radioCount].name);
-    		radioCount++;
-    		radioValues = 0;
-    	}
-    	
-    	strcpy(RadioGroups[radioCount-1].values[radioValues], tmpValue);
-    	radioValues++;
+    	lr_continue_on_error(1);
+    	web_submit_data(indexForm,
+	        url,
+	        "Method=POST",
+			"Snapshot=t17.inf",
+			ITEMDATA,
+			FormFields[1].name, FormFields[1].value, ENDITEM,
+			FormFields[2].name, FormFields[2].value, ENDITEM,
+			FormFields[3].name, FormFields[3].value, ENDITEM,
+			FormFields[4].name, FormFields[4].value, ENDITEM,
+			FormFields[5].name, FormFields[5].value, ENDITEM,
+			FormFields[6].name, FormFields[6].value, ENDITEM,
+			FormFields[7].name, FormFields[7].value, ENDITEM,
+			FormFields[8].name, FormFields[8].value, ENDITEM,
+			FormFields[9].name, FormFields[9].value, ENDITEM,
+			FormFields[10].name, FormFields[10].value, ENDITEM,
+			LAST);
+	    
+	    radioCount = 1;
+	    radioValues = 0;
+		selectCount = 1;
+		selectCount = 0;
+	
+		lr_continue_on_error(0);
     }
-    
-    for (iR = 1; iR < radioCount; iR++){
-    	char max[STR_LEN];
-    	strcpy(max,RadioGroups[iR].values[0]);
-    	for (radioValues = 0; radioValues < 10; radioValues++)
-    	{
-    		if(strlen(RadioGroups[iR].values[radioValues]) > strlen(max))
-    		{
-    			strcpy(max,RadioGroups[iR].values[radioValues]);
-    		}
-    		strcpy(RadioGroups[iR].maxValue, max);
-    	}
-    }
-    
-    j = radioCount - 1;
-    for (i = iT; i < iT + radioCount - 1; i++)
-    {
-		strcpy(FormFields[i].name, "Name=");
-		strcat(FormFields[i].name, RadioGroups[j].name);
-		strcpy(FormFields[i].value, "Value=");
-		strcat(FormFields[i].value, RadioGroups[j].maxValue);
-		lr_output_message(FormFields[i].name);
-		lr_output_message(FormFields[i].value);
-		j--;
-    }
-    
-    selectCount = atoi(lr_eval_string("{Select_count}"));
-                       
-    for (iS = 1; iS <= selectCount; iS++)
-    {
-    	int startPos = 0;
-    	int endPos = 0;
-    	int index, i = 0;
-    	char tmpStr[1024];
-    	sprintf(tmpSelectFields[iS], "{Select_%d}", iS);
-    	strcpy(tmpStr, lr_eval_string(tmpSelectFields[iS]));
-    	
-    	for( index = 0; index < strlen(tmpStr); index++ )
-    	{
-    		if (tmpStr[index] == '"')
-    		{
-    			startPos = index;
-    			while((tmpStr[index+1] != '"') && (index < strlen(tmpStr) - 1))
-    			{
-    				index++;
-    			}
-    			endPos = index;
-    			
-    			
-    			if (startPos == 0)
-    			{
-    				strncpy(SelectBoxes[iS].name, tmpStr+startPos + 1, endPos - startPos);
-    			}
-    			else
-    			{
-    				strncpy(SelectBoxes[iS].values[i], tmpStr+startPos + 1, endPos - startPos);
-    				i++;
-    			}
-    			index++;
-    		}
-    	}
-    	
-    }
-    
-    
-    for (iS = 1; iS <= selectCount; iS++)
-    {
-    	char max[STR_LEN];
-    	strcpy(max,SelectBoxes[iS].values[0]);
-    	for (selectValues = 0; selectValues < 10; selectValues++)
-    	{
-    		if(strlen(SelectBoxes[iS].values[selectValues]) > strlen(max))
-    		{
-    			strcpy(max,SelectBoxes[iS].values[selectValues]);
-    		}
-    		strcpy(SelectBoxes[iS].maxValue, max);
-    	}
-    }
-
-    j = selectCount;
-    for (iT = i; iT < i + selectCount; iT++)
-    {
-		strcpy(FormFields[iT].name, "Name=");
-		strcat(FormFields[iT].name, SelectBoxes[j].name);
-		strcpy(FormFields[iT].value, "Value=");
-		strcat(FormFields[iT].value, SelectBoxes[j].maxValue);
-		lr_output_message(FormFields[iT].name);
-		lr_output_message(FormFields[iT].value);
-		j--;
-    }
-    
-    strcpy(FormFields[iT].name, "LAST");
-
-	web_reg_find("Text=Question 2", 
-		LAST);
-
-	web_submit_data("1",
-        "Action=http://test.youplace.net/question/1",
-        "Method=POST",
-		"Snapshot=t17.inf",
-		ITEMDATA,
-		FormFields[1].name, FormFields[1].value, ENDITEM,
-		FormFields[2].name, FormFields[2].value, ENDITEM,
-		FormFields[3].name, FormFields[3].value, ENDITEM,
-		FormFields[4].name, FormFields[4].value, ENDITEM,
-		FormFields[5].name, FormFields[5].value, ENDITEM,
-		FormFields[6].name, FormFields[6].value, ENDITEM,
-		FormFields[7].name, FormFields[7].value, ENDITEM,
-		FormFields[8].name, FormFields[8].value, ENDITEM,
-		FormFields[9].name, FormFields[9].value, ENDITEM,
-		FormFields[10].name, FormFields[10].value, ENDITEM,
-		LAST);
-    
-    radioCount = 1;
-    radioValues = 0;
-	selectCount = 1;
-	selectCount = 0;
-	web_reg_find("Text=Question 3", 
-		LAST);
-
-	web_submit_form("2", 
-		"Snapshot=t18.inf", 
-		ITEMDATA, 
-		"Name=PMwaUUYPE5oYfLgR", "Value=6rdm2K84y4gKj40", ENDITEM, 
-		"Name=icOQvyqId1KVanQH", "Value=test", ENDITEM, 
-		"Name=NLjwu1tvfGhjC1rV", "Value=test", ENDITEM, 
-		LAST);
-
-	web_reg_find("Text=Question 4", 
-		LAST);
-
-	web_submit_form("3", 
-		"Snapshot=t19.inf", 
-		ITEMDATA, 
-		"Name=vQd3s12phAZIDJWD", "Value=iK1JQSiI5ehnLC", ENDITEM, 
-		LAST);
-
-	web_reg_find("Text=Question 5", 
-		LAST);
-
-	web_submit_form("4", 
-		"Snapshot=t20.inf", 
-		ITEMDATA, 
-		"Name=SgiWKXil5CgIpS93", "Value=u3muTlY6Gx9l6C9", ENDITEM, 
-		"Name=e9RHlNuCoZGAsrXl", "Value=nJ4L1JYT95N9yA", ENDITEM, 
-		"Name=t87b2WEhwlhweBa6", "Value=Ek5tSoggyCq2Bqun", ENDITEM, 
-		"Name=7CEriolt5bd1jSF2", "Value=test", ENDITEM, 
-		LAST);
-
-	web_set_sockets_option("SSL_VERSION", "TLS1.2");
-
-	web_reg_find("Text=Question 6", 
-		LAST);
-
-	web_submit_form("5", 
-		"Snapshot=t21.inf", 
-		ITEMDATA, 
-		"Name=ykoK5QlzgjQd40cU", "Value=test", ENDITEM, 
-		"Name=hvwWoU8RcFTVE0rV", "Value=CNYqsNwZ64EuC", ENDITEM, 
-		LAST);
-
-	web_reg_find("Text=Question 7", 
-		LAST);
-
-	web_submit_form("6", 
-		"Snapshot=t22.inf", 
-		ITEMDATA, 
-		"Name=j3MKV6aaGnnAD9a6", "Value=AJFtb3M1ajQm6", ENDITEM, 
-		"Name=va9gD49hI7JIYsK8", "Value=gc6de0OqxKVbj", ENDITEM, 
-		"Name=vbDjcraz7mww9JCe", "Value=test", ENDITEM, 
-		LAST);
-
-	web_reg_find("Text=Question 8", 
-		LAST);
-
-	web_submit_form("7", 
-		"Snapshot=t23.inf", 
-		ITEMDATA, 
-		"Name=EiepiWDTq6A2yMCn", "Value=test", ENDITEM, 
-		"Name=Z8nWzjta4W1l97aR", "Value=069RWX41uHPb5Q", ENDITEM, 
-		LAST);
-
-	web_reg_find("Text=Question 9", 
-		LAST);
-
-	web_submit_form("8", 
-		"Snapshot=t24.inf", 
-		ITEMDATA, 
-		"Name=Nfz5NWwsQVyTZPdc", "Value=ULY3APEvUwdKh1", ENDITEM, 
-		"Name=sGfrC0mHOnC3rI5P", "Value=rz4ckv2SaHCMZz", ENDITEM, 
-		"Name=cO7APX1TCXUkG1nw", "Value=test", ENDITEM, 
-		LAST);
-
-	web_reg_find("Text=Test successfully passed", 
-		LAST);
-
-	lr_think_time(4);
-
-	web_submit_form("9", 
-		"Snapshot=t25.inf", 
-		ITEMDATA, 
-		"Name=6x2dmWFQCgc2G7zG", "Value=test", ENDITEM, 
-		"Name=7pCpqiDRETjhWMD5", "Value=test", ENDITEM, 
-		"Name=IXn1NS7DRdMoRrnQ", "Value=9RW7Zyybck66tqiN", ENDITEM, 
-		LAST);
 
 	return 0;
 }
